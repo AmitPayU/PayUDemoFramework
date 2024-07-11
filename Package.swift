553ceb8fb6554c84bd8bbb84d88d73618fa6ca88
Amit Salaria <amit.salaria@payu.in>
2024-07-11 13:46:48 +0530

    no message

--- Package.swift
+++ Package.swift
@@ -1,13 +1,21 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
 let package = Package(
     name: "PayUDemoFramework",
-    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PayUDemoFramework",
             targets: ["PayUDemoFramework"]),
     ],
     targets: [
-        .binaryTarget(name: "PayUDemoFramework", path: "./PayUDemoFramework.xcframework")
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        
+        .binaryTarget(name: "PayUDemoFramework", path: "./Sources/PayUDemoFramework.xcframework"),
+
     ]
 )
